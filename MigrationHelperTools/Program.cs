fe459630c75563f10d2f594bb6b38acf66b1710c
svCcare <maciej.pawlak94@gmail.com>
2022-06-12 20:10:44 +0200

    Add project files.

---
+++ MigrationHelperTools/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MigrationHelperTools
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Clipboard.SetDataObject(DateTime.Now.ToString("yyyyMMddHHmm"), true, 2, 20);
+        }
+    }
+}
